74549cac007f5ef96eb135a896146790337a8fba
Nag <nagcloudlab@gmail.com>
2024-02-19 13:01:34 +0530

    day-1 Ex

---
+++ 01-play-with-python/Ex1.py
@@ -0,0 +1,16 @@
+
+csvReport=[
+    "Emp1",1000.00,"Dep1",
+    "Emp2",2000.00,"Dep2",
+    "Emp3",3000.00,"Dep1",
+    "Emp4",4000.00,"Dep4",
+    "Emp5",5000.00,"Dep3",
+]
+
+# find total salary of each department
+
+# e.g
+
+# Dep1: 4000.00
+# Dep2: 2000.00
+# Dep3: 5000.00
